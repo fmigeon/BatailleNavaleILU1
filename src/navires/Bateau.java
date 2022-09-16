170c1d308ddb23da7b671fa94ca5f3c2297f5693
Frederic Migeon <frederic.migeon@irit.fr>
2022-09-16 17:47:23 +0200

    Added Bateau

---
+++ src/navires/Bateau.java
@@ -0,0 +1,17 @@
+package navires;
+// @ Author : Chaudet
+// @ Author : Migeon
+
+public class Bateau {
+	private String nom;
+	private int tailleBateau;
+
+	public Bateau(String nom, int tailleBateau) {
+		this.nom = nom;
+		this.tailleBateau = tailleBateau;
+	}
+
+	public int getTailleBateau() {
+		return tailleBateau;
+	}
+}
